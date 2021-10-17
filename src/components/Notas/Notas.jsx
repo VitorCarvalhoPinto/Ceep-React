9a6c881b4af5bea88ccd45ceccfd0f0dacaf928d
Vitor_Carvalho <mrdark.vc@gmail.com>
2021-10-17 17:59:15 -0300

    finalização

--- src/components/Notas/Notas.jsx
+++ src/components/Notas/Notas.jsx
@@ -15,7 +15,6 @@ export default class Notas extends Component {
                     <h3 key={this.props.indice} className="">{this.props.titulo}</h3>
                 </header>
                 <p className="">{this.props.texto}</p>
-                <i className="fa fa-check fa-sm" aria-hidden="true"></i>
                 <i onClick={this.apagar.bind(this)} className="fa fa-trash fa-sm" aria-hidden="true"></i>    
             </section>
         );
